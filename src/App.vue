015218609eaa60141950baff6a606977f1fa7b6e
xiaoduoduoqy <492300924@qq.com>
2020-11-17 18:37:53 +0800

    修改相关控件

--- src/App.vue
+++ src/App.vue
@@ -10,9 +10,9 @@ import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  // components: {
+  //   HelloWorld
+  // }
 }
 </script>
 
