e9e0c219913172b2694ef8a8070c971f1b8a5982
xiaoduoduoqy <492300924@qq.com>
2020-11-18 17:05:59 +0800

    滚动窗的组件封装

---
+++ src/components/common/swiper/Swiper.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="hy-swiper">
+    <div class="swiper" @touchstart="touchStart" @touchmove="touchMove" @touchend="touchEnd">
+      <slot></slot>
+    </div>
+    <slot name="indicator">
+    </slot>
+    <div class="indicator">
+      <slot name="indicator" v-if="showIndicator && slideCount>1">
+<!--        <div v-for="(item, index) in slideCount" class="indi-item" :class="{active: index === currentIndex-1}"-->
+<!--             :key="index"></div>-->
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Swiper"
+}
+</script>
+
+<style scoped>
+
+</style>
