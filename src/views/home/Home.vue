92999b918fa831e699945b15a6a438cb5b9c046e
xiaoduoduoqy <492300924@qq.com>
2020-11-18 15:06:41 +0800

    界面的导航组件封装完成

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,13 +1,26 @@
 <template>
-<h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nva">
+      <div slot="center">购物街</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+
+import NavBar from "components/common/navbar/NavBar";
+
 export default {
-name: "Home"
+  name: "Home",
+  components: {
+    NavBar
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nva {
+  background-color: var(--color-tint);
+  color: #ffffff;
+}
 </style>
