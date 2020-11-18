2ce306784eb742aa25bd14f72081d1f7f770d5ed
xiaoduoduoqy <492300924@qq.com>
2020-11-18 17:26:47 +0800

    初步相关的轮播图写入

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,21 @@
+<template>
+  <swiper-item>
+
+  </swiper-item>
+</template>
+
+<script>
+import {Swiper, SwiperItem} from 'components/common/swiper';
+
+export default {
+  name: "HomeSwiper",
+  components{
+    Swiper,
+    SwiperItem
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
